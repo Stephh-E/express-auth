1caf4458ea914e0fef89a3612364a46f19bdc782
Steph Elsley <161695703+Stephh-E@users.noreply.github.com>
2024-11-07 11:56:36 +1100

    fixed syntax

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+require("dotenv").config();
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+// module.exports = { app }
+const { dbConnect } = require("./functions/dbFunctions.js");
 const {app} = require("./server.js");
 
-require("dotenv").config();
+// module.exports = app;
+// const app = require()
+
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log("Server is running on port http://localhost:" + PORT);
+app.listen(PORT, async () => {
+	await dbConnect();
+	console.log("Server is running on port http://localhost:" + PORT);
 });
--- src/server.js
+++ src/server.js
@@ -1,39 +1,55 @@
 const express = require("express");
+const { User } = require("./models/UserModel");
+const { generateJWT, validateUserAuth } = require("./functions/jwtfunctions");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-    response.json({
-        message: "Hello, world 12!"
-    });
+// Enable this if you want front-ends to have more freedom about how they make requests
+// eg. this is for HTML URL-encoded forms 
+// app.use(express.urlencoded({extended: true}));
 
+app.get("/", (request, response) => {
+	response.json({
+		message:"Hello, world!"
+	});
 });
 
 app.post("/signup", async (request, response) => {
-    let username = request.body.username;
-    let password = request.body.password;
+	// check that a username and password are provided in request.body 
+	let username = request.body.username;
+	let password = request.body.password;
 
-    if (!username || !password) {
-        response.status(400).json({
-            message: "Incorrect or missing sign-up credentials provided."
-        });
-    }
+	if (!username || !password) {
+		response.status(400).json({
+			message:"Incorrect or missing sign-up credentials provided."
+		});
+	}
 
-    let newUser = await User.create({username: username, password: password});
 
-    let newJWT = generateJWT(newUser.id, newUser.username);
+	// make a user in the DB using the username and password
+	let newUser = await User.create({username: username, password: password});
 
-    response.json({
-        jwt:newJWT,
-        user:{
-            id: newUser
-        }
-    });
+	// make a JWT based on the username and userID 
+	let newJwt = generateJWT(newUser.id, newUser.username);
 
+	// return the JWT 
+	response.json({
+		jwt: newJwt,
+		user: {
+			id: newUser.id,
+			username: newUser.username
+		}
+	});
 });
 
+app.get("/protectedRoute", validateUserAuth, (request, response) => {
+	response.json({
+		message:"You can see protected content because you're signed in!"
+	});
+})
+
 module.exports = {
-    app
+	app
 }
